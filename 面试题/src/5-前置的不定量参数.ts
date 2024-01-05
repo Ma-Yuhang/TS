cd95d8c2d75599a71937751bc7dcfee2da662c43
189690 <2312165827@qq.com>
2024-01-05 15:38:00 +0800

    feat: 5-前置的不定量参数2

--- 面试题/src/5-前置的不定量参数.ts
+++ 面试题/src/5-前置的不定量参数.ts
@@ -1,3 +1,5 @@
+export {};
+
 type BasicType = keyof BasicTypeMap;
 type BasicTypeMap = {
   string: string;
@@ -17,4 +19,5 @@ declare function addImpl<T extends BasicType[]>(
 ): void;
 
 // 有这么一个函数，接收不确定个参数，最后一个参数是一个函数，但是前边的参数是跟最后一个函数的参数类型一致
+// 'string', 'number', 'boolean' => (a: string, b: number, c, :boolean)
 addImpl('number', 'boolean', 'string', (a, b, c) => {});
