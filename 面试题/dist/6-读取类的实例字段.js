8d1563b3bbd93fe857f8aff03beb8c9bd42906d2
mayuhang <2312165827@qq.com>
2024-03-23 21:37:48 +0800

    feat: 读取类的实例字段

---
+++ 面试题/dist/6-读取类的实例字段.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class A {
+    constructor() {
+        this.a = 1;
+        this.b = '';
+    }
+}
+const a = 'a';
+console.log(a);
