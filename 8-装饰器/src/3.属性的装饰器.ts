3ca7259024c85234d8a4890a9ee7a176af9a41ca
189690 <yuhang.ma@cn.chervongroup.com>
2024-06-18 19:04:50 +0800

    chore: 修改属性装饰器参数描述

--- 8-装饰器/src/3.属性的装饰器.ts
+++ 8-装饰器/src/3.属性的装饰器.ts
@@ -1,4 +1,5 @@
-// 参数跟方法装饰器参数一样，但没有第三个参数
+// 参数1: 如果是实例属性，参数1就是类的原型对象，如果是静态属性，就是类本身
+// 参数2: 属性名
 function nameDecorator(target: any, key: string): any {
   console.log(target);
   console.log(key);
@@ -17,5 +18,5 @@ class Test2 {
 }
 
 let p = new Test2();
-p.uname = 'da'
+// p.uname = 'da'
 console.log(p.uname);
